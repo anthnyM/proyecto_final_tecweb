8885c4ae249eed8927ca186f5268eabb596257fa
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-11-30 23:05:12 -0400

    Add Filters for Browsing

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -3,11 +3,12 @@ import { BrowseService } from '../../services/browse.service';
 import { Pokemon } from '../../models/pokemon';
 import { PokemonComponent } from '../../elements/pokemon/pokemon.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [PokemonComponent, CommonModule],
+  imports: [PokemonComponent, CommonModule, FormsModule],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss'
 })
@@ -15,6 +16,12 @@ export class BrowseComponent implements OnInit{
   pokemonList: Pokemon[] = [];
   error: string | null = null;
   isLoading = true;
+  filters = {
+    type: '',
+    generation: null as number | null,
+    legendary: false,
+    name: ''
+  };
 
   constructor(private browseService: BrowseService) {}
 
@@ -33,6 +40,17 @@ export class BrowseComponent implements OnInit{
         this.isLoading = false;
       }
     });
+  }
+
+  applyFilters(): void {
+    this.browseService.getPokemonFiltered(this.filters).subscribe(
+      (data) => {
+        this.pokemonList = data;
+      },
+      (error) => {
+        console.error('Error fetching pokemon: ', error);
+      }
+    );
   }
 
 }
--- src/app/services/browse.service.ts
+++ src/app/services/browse.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pokemon } from '../models/pokemon';
 @Injectable({
   providedIn: 'root'
@@ -12,5 +12,22 @@ export class BrowseService {
 
   getAllPokemon(): Observable<Pokemon[]>{
     return this.http.get<Pokemon[]>(this.url)
+  }
+
+  getPokemonFiltered(filters: {type?: string, generation?: number; legendary?: boolean, name?: string}): Observable<Pokemon[]>{
+    let params = new HttpParams();
+    if (filters.type) {
+      params = params.set('type', filters.type);
+    }
+    if (filters.generation) {
+      params = params.set('generation', filters.generation.toString());
+    }
+    if (filters.legendary !== undefined) {
+      params = params.set('legendary', filters.legendary.toString());
+    }
+    if (filters.name) {
+      params = params.set('name', filters.name);
+    }
+    return this.http.get<Pokemon[]>(this.url + '/filter/', {params});
   }
 }
